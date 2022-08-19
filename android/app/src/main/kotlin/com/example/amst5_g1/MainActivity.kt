a7a7ab2207295643a9cbc7e2d02571d94a7bf95e
Alex Mora <aleemora@espol.edu.ec>
2022-08-18 20:36:29 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/amst5_g1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.amst5_g1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:amst5_g1/shared/nav.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+  class MyApp extends StatefulWidget {
+    _AppState createState() => _AppState();
+}
+
+class _AppState extends State<MyApp> {
+  final GlobalKey<NavigatorState> navigatorKey =
+  new GlobalKey<NavigatorState>();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vacheck',
+      navigatorKey: navigatorKey,
+      initialRoute: 'nav',
+      routes: {
+        'nav': (context) => Nav(),
+      },
+      theme: ThemeData(
+        primaryColor: Colors.red[400],
+      ),
+    );
+  }
+}
+
